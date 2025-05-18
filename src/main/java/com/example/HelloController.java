d476f169f09fbf628e50aeb814a85d8dd08e218d
Toshiaki Maki <makingx@gmail.com>
2025-05-18 14:17:38 +0900

    Use Reactor gRPC

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,35 +1,32 @@
 package com.example;
 
 import com.example.proto.HelloRequest;
-import com.example.proto.HelloResponse;
-import com.example.proto.HelloServiceGrpc;
-import com.google.common.collect.Streams;
-import java.util.Iterator;
-import java.util.List;
+import com.example.proto.ReactorHelloServiceGrpc;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloController {
 
-    private final HelloServiceGrpc.HelloServiceBlockingStub helloServiceStub;
+    private final ReactorHelloServiceGrpc.ReactorHelloServiceStub helloServiceStub;
 
-    public HelloController(HelloServiceGrpc.HelloServiceBlockingStub helloServiceStub) {
+    public HelloController(ReactorHelloServiceGrpc.ReactorHelloServiceStub helloServiceStub) {
         this.helloServiceStub = helloServiceStub;
     }
 
     @GetMapping(path = "/")
-    public Reply sayHello(@RequestParam String greeting) {
-        HelloResponse response = helloServiceStub.sayHello(HelloRequest.newBuilder().setGreeting(greeting).build());
-        return new Reply(response.getReply());
+    public Mono<Reply> sayHello(@RequestParam String greeting) {
+        return helloServiceStub.sayHello(HelloRequest.newBuilder().setGreeting(greeting).build())
+            .map(r -> new Reply(r.getReply()));
     }
 
     @GetMapping(path = "/lots-of-replies")
-    public List<Reply> lotsOfReplies(@RequestParam String greeting) {
-        Iterator<HelloResponse> replies = helloServiceStub
-            .lotsOfReplies(HelloRequest.newBuilder().setGreeting(greeting).build());
-        return Streams.stream(replies).map(r -> new Reply(r.getReply())).toList();
+    public Flux<Reply> lotsOfReplies(@RequestParam String greeting) {
+        return helloServiceStub.lotsOfReplies(HelloRequest.newBuilder().setGreeting(greeting).build())
+            .map(r -> new Reply(r.getReply()));
     }
 
     public record Reply(String reply) {
